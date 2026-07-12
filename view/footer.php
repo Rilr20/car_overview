15cd89aa7d320542b7629116c9eb81dfc9aa05b3
Rilr20 <nrrl99@gmail.com>
2026-07-12 19:51:25 +0200

    url change

--- view/footer.php
+++ view/footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="divider"></div>
     <div class="footer-content">
-        <p>&copy; <?= date('Y'); ?> RILR20</p>
-        <a href="https://github.com/Rilr20/car_overview/" target="_blank" rel="noopener noreferrer" class="github-link">
+        <p>&copy;<?= date('Y'); ?> RILR20</p>
+        <a target="_blank" href="https://github.com/Rilr20/" target="_blank" rel="noopener noreferrer" class="github-link">
             GitHub
         </a>
     </div>
