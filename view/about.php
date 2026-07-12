af36986a9f224f604c8dc8aae26af49e52fb6832
Rilr20 <nrrl99@gmail.com>
2026-07-12 19:50:27 +0200

    added text in about an index pages

--- view/about.php
+++ view/about.php
@@ -1,2 +1,8 @@
-
 <h2 class="page-title">About</h2>
+<p>Written in pure php!</p>
+<p>The webscraper takes the information from bilweb and saves it directly into a MySQL database.</p>
+<p>
+    The stored data is then used by this application for an AJAX-based search form. With the following parameters: brand, model year, and registration number.
+</p>
+
+<a target="_blank" href="https://github.com/Rilr20/car_overview">View the Github Repository</a>
--- view/home.php
+++ view/home.php
@@ -1,2 +1,2 @@
 <h2 class="page-title">Home</h2>
-
+<p>The scraped car listings from over 1000 bilweb pages have been used to create the search functionality!</p>
