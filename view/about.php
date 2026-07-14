e22e390ee8e4c45d6bc14e83ee0618ebb6e8b485
Rilr20 <nrrl99@gmail.com>
2026-07-14 21:33:16 +0200

    info about deployment

--- view/about.php
+++ view/about.php
@@ -1,8 +1,12 @@
 <h2 class="page-title">About</h2>
 <p class="center">Written in pure php!</p>
+<br>
 <p>The webscraper takes the information from bilweb and saves it directly into a MySQL database.</p>
+<br>
 <p>
     The stored data is then used by this application for an AJAX-based search form. With the following parameters: brand, model year, and registration number.
 </p>
+<br>
+<p> Deployed on a laptop repurposed as an Ubuntu Server. The application runs on Nginx with MariaDB, with GitHub Actions handling automated deployments after each commit. </p>
 
 <a target="_blank" href="https://github.com/Rilr20/car_overview">View the Github Repository</a>
