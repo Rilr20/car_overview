bb9bdef5549c9652e2d2de07ce03ad703821a334
Rilr20 <nrrl99@gmail.com>
2026-07-15 15:01:57 +0200

    centers the text

--- view/multipage.php
+++ view/multipage.php
@@ -5,7 +5,7 @@
             <?php if ($page) : ?>
                 <?php require $page["file"] ?>
             <?php else : ?>
-                <p>You have selected a page reference '<?= htmlentities($pageReference) ?>' that does not map to an actual page.</p>
+                <p class="center">You have selected a page reference '<?= htmlentities($pageReference) ?>' that does not map to an actual page.</p>
             <?php endif; ?>
         </article>
     </main>
